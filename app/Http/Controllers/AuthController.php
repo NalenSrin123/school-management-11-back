5492d2c44682aa725274465e4c035aa495ba803f
Cherry <cherry@Cherrys-MacBook-Air.local>
2026-03-12 18:23:02 +0700

    hehehe a little bit change column

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -13,34 +11,40 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            "name"     => "required|string|max:255",
-            "email"    => "required|string|email|max:255|unique:users",
+            "name" => "required|string|max:255",
+            "email" => "required|string|email|max:255|unique:users",
             "password" => [
                 "required",
                 "confirmed",
-                Password::min(8)->letters()->mixedCase()->numbers()->symbols(),
+                Password::min(6)
+                    ->letters()
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols(),
             ],
         ]);
 
         if ($validator->fails()) {
             return response()->json([
                 "message" => "Validation failed!",
-                "errors"  => $validator->errors(),
+                "errors" => $validator->errors(),
             ], 422);
         }
 
         $validated = $validator->validated();
 
         $user = User::create([
-            "name"     => $validated["name"],
-            "email"    => $validated["email"],
+            "name" => $validated["name"],
+            "email" => $validated["email"],
             "password" => Hash::make($validated["password"]),
+            "status" => true,
+            "role_id" => 1,
         ]);
 
         return response()->json([
             "message" => "User registered successfully",
-            "data"    => [
-                "user"  => $user->only(["id", "name", "email"]),
+            "data" => [
+                "user" => $user->only(["id", "name", "email"]),
             ],
         ], 201);
     }
